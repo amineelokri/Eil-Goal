014948c6c000714fe334ce90fc3748ba658a775c
mohamedbougra <mohamed.bougra@edu.uca.ma>
2024-03-26 03:30:51 +0100

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,17 @@
-import { Component, inject } from "@angular/core";
-import { Auth, User, user, authState, idToken } from "@angular/fire/auth";
-import { Subscription } from "rxjs";
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { Component, OnInit } from '@angular/core';
+import { APIService } from './api.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-  //This part helps change dynamically the login form to the forgotten password Form
-  showForgottenPasswordForm: boolean = false;
-  toggleForgottenPasswordForm() {
-    this.showForgottenPasswordForm = !this.showForgottenPasswordForm;
-  }
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-    })
-    .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-    });
-  
-  user$ = user(this.auth);
-  userSubscription: Subscription;
-  title = "eilgoal";
+export class AppComponent implements OnInit {
 
-  constructor(auth: Auth, private snackBar: MatSnackBar) {
-    this.userSubscription = this.user$.subscribe((aUser: User | null) => {
-      //handle user state changes here. Note, that user will be null if there is no currently logged in user.
-      console.log(aUser);
-    });
-  }
 
-  ngOnDestroy() {
-    // when manually subscribing to an observable remember to unsubscribe in ngOnDestroy
-    this.userSubscription.unsubscribe();
-  }
-  
+  title = 'webservices2024';
+
+  ngOnInit() {}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import '@angular/compiler'
+import {provideAuth, getAuth} from '@angular/fire/auth'
+import {provideFirestore, getFirestore} from '@angular/fire/firestore'
+import { HttpClientModule } from '@angular/common/http';
+//PRIME NG COMPONENTS
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"eilgoal","appId":"1:451422785043:web:c5550f56e6ad81ff4e39e6","storageBucket":"eilgoal.appspot.com","apiKey":"AIzaSyAl92ytU9-bU7PovpIUhWe6RvfHj-PC5ow","authDomain":"eilgoal.firebaseapp.com","messagingSenderId":"451422785043"})),
-    provideAuth(() => {
-      const auth = getAuth();
-      connectAuthEmulator(auth, 'http://localhost:4200/', { disableWarnings: true });
-      return auth;
-    }),
-    provideFirestore(() => getFirestore())
+    provideFirebaseApp( () =>initializeApp({
+      projectId: 'testeilco2024',
+      appId: '1:410339903646:web:1d0269e8cfa24fae79538f',
+      databaseURL:
+        'https://testeilco2024-default-rtdb.europe-west1.firebasedatabase.app',
+      storageBucket: 'testeilco2024.appspot.com',
+      apiKey: 'AIzaSyB8uRqx53oxEvV-un9ImNrT0cm2nx4Wu8c',
+      authDomain: 'testeilco2024.firebaseapp.com',
+      messagingSenderId: '410339903646',
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
